6b1c1a8cc84462d747ac3b37269a03ed71901ed6
eric-delarosa <ericd.delarosa@gmail.com>
2022-08-05 17:03:50 -0400

    Fix grid creation method to fit the conatiner size while changing internal square size

--- app.js
+++ app.js
@@ -1,14 +1,13 @@
+const DEFAULT_SIZE = 16;
+
+let currentSize = DEFAULT_SIZE;
+
 const gridArea = document.querySelector('.grid-area');
 
-// const slider = document.getElementById('gridRange');
-// const output = document.getElementById('squareValue');
-// output.textContent = slider.value;
+const slider = document.getElementById('gridRange');
+const output = document.getElementById('squareValue');
 
-// function getSliderValue() {
-//     slider.oninput = function () {
-//         output.textContent = this.value;     
-//     }
-// }
+slider.onchange = (e) => changeSize(e.target.value);
 
 const squareValue = document.getElementById('squareInput');
 
@@ -22,17 +21,39 @@ const colorButton = document.querySelector('#color');
 colorButton.addEventListener("click", () => color());
 
 
-createGrid(squareValue.value);
+function setupGrid(size) {
+    gridArea.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
+    gridArea.style.gridTemplateRows = `repeat(${size}, 1fr)`;
 
-
-function createGrid(numSquares) {
-    for (let row = 0; row < numSquares; row++) {
-      for (let col = 0; col < numSquares; col++) {
+    for (let i = 0; i < size * size; i++) {
         const square = document.createElement("div");
         square.setAttribute("class", "grid-square");
         gridArea.appendChild(square);
-      }
     }
+    
+}
+
+function reloadGrid() {
+  clearGrid();
+  setupGrid(currentSize);
+}
+
+function clearGrid() {
+  gridArea.innerHTML = '';
+}
+
+function updateSizeValue(value) {
+    output.textContent = `${value} x ${value}`
+}
+
+function changeSize(value){
+    setCurrentSize(value);
+    updateSizeValue(value);
+    reloadGrid(); 
+}
+
+function setCurrentSize(newSize) {
+    currentSize = newSize;
 }
 
 function draw() {
@@ -73,4 +94,7 @@ function color() {
     }    
 }
 
-draw();
+window.onload = () => {
+    setupGrid(DEFAULT_SIZE);
+    draw();
+};
